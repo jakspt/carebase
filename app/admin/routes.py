2c8038970f71695498cf157cd60543bb40431be0
Jakob Sapototzky <32401158+jakspt@users.noreply.github.com>
2025-12-20 21:25:14 +0100

    added admin page

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -7,3 +7,19 @@ from . import admin_bp
 @admin_bp.route('/')
 def overview():
     return render_template('admin/overview.html')
+
+
+@admin_bp.route('/fill_db', methods=['POST'])
+def fill_db():
+    print('clearing db ...')
+    print('filling db ...')
+    # TODO: Implement DB filling
+    return overview()
+
+
+@admin_bp.route('/migrate_db', methods=['POST'])
+def migrate_db():
+    print('migrating db ...')
+    print('now using Mongooo')
+    # TODO: Implement DB migration
+    return overview()
