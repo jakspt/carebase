58bd251b5b8ea309405e8a257098e3846fc97c3f
Jakob Sapototzky <32401158+jakspt@users.noreply.github.com>
2025-12-20 17:41:50 +0100

    added navbar

--- app/doctor/routes.py
+++ app/doctor/routes.py
@@ -2,6 +2,7 @@ from flask import render_template
 from . import doctor_bp
 
 
+# TODO: Implement
 @doctor_bp.route('/')
-def dashboard():
-    return render_template('doctor/doctor.html')
+def overview():
+    return render_template('doctor/overview.html')
