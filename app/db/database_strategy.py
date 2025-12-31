477f298de5f5910825d32a8623d02ea1be9fd72a
Jakob Sapototzky <32401158+jakspt@users.noreply.github.com>
2025-12-31 15:49:37 +0100

    changes to function defs in strategy

--- app/db/database_strategy.py
+++ app/db/database_strategy.py
@@ -9,17 +9,27 @@ class DatabaseStrategy(ABC):
 
     # --- DOCTOR USE CASE (User A), examples ---
     @abstractmethod
-    def search_patient(self, name: str) -> list: pass
+    def find_patients(self, query: str) -> list[dict]: pass
+
+    # get details plus all appointments
+    @abstractmethod
+    def get_patient_details(self, patient_id: int) -> list: pass
 
     @abstractmethod
-    def add_treatment(self, appt_id: int, desc: str) -> bool: pass
+    def get_all_med_names(self) -> list[str]: pass
+
+    @abstractmethod
+    def get_med_id_by_name(self, name: str) -> int: pass
+
+    @abstractmethod
+    def add_treatment(self, patient_id: int, appt_id: int, desc: str, cost: float, meds: list[str]) -> bool: pass
 
     # --- CLERK USE CASE (User B), examples ---
-    @abstractmethod
-    def generate_revenue_report(self) -> dict: pass
-
-    @abstractmethod
-    def get_hospital_stats(self) -> dict: pass
+    # @abstractmethod
+    # def generate_revenue_report(self) -> dict: pass
+    #
+    # @abstractmethod
+    # def get_hospital_stats(self) -> dict: pass
 
     # --- Shared functionality, implement in MongoBase/SQLBase
     @abstractmethod
--- app/db/sql/doctor_mixin.py
+++ app/db/sql/doctor_mixin.py
@@ -1,3 +1,16 @@
 class SQLDoctorMixin:
-    # implement all doctor methods here
-    pass
+    def find_patients(self, query: str) -> list[dict]:
+        pass
+
+    # get details plus all appointments
+    def get_patient_details(self, patient_id: int) -> list:
+        pass
+
+    def get_all_med_names(self) -> list[str]:
+        pass
+
+    def get_med_id_by_name(self, name: str) -> int:
+        pass
+
+    def add_treatment(self, patient_id: int, appt_id: int, desc: str, cost: float, meds: list[str]) -> bool:
+        pass
