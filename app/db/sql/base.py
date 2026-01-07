2acefb531000fe5c5ac59480907e53be409f805a
Jakob Sapototzky <32401158+jakspt@users.noreply.github.com>
2026-01-07 13:53:35 +0100

    finalized db changes: batch insert, appts now per patient and medications use more creative names

--- app/db/sql/base.py
+++ app/db/sql/base.py
@@ -2,16 +2,15 @@ import mariadb
 
 
 class SQLBase:
-
     def _get_connection(self):
         try:
             connection = mariadb.connect(
-                user='root',
-                password='1234',
-                host='localhost',
+                user="root",
+                password="1234",
+                host="localhost",
                 port=3306,
-                database='carebase'
+                database="carebase",
             )
             return connection
         except mariadb.Error as e:
-            print(f"Error connecting to MariaDB: {e}")
+            raise Exception(f"Error connecting to MariaDB, Reason: {e}")
