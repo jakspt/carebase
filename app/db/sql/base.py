a97e0db2b1cf0214a0d62acc834fe8ea5197b5a4
Harry Ziqi Ye <a12230224@univie.ac.at>
2026-01-09 17:34:49 +0100

    actually

--- app/db/sql/base.py
+++ app/db/sql/base.py
@@ -7,7 +7,7 @@ class SQLBase:
             connection = mariadb.connect(
                 user="root",
                 password="1234",
-                host="localhost",
+                host="127.0.0.1",
                 port=3306,
                 database="carebase",
             )
