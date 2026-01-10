dead114b24f073919ef4e5fcba0c2a308095c0f8
Jakob Sapototzky <32401158+jakspt@users.noreply.github.com>
2026-01-11 00:18:45 +0100

    comment cleanup

--- app/db/database_strategy.py
+++ app/db/database_strategy.py
@@ -2,17 +2,11 @@ from abc import ABC, abstractmethod
 
 
 class DatabaseStrategy(ABC):
-    """
-    The Interface: All database strategies must implement these methods.
-    => Abstraktionen auf Domain-Level halten, u.a. wg. Embedding auf MongoDB dann (1 table != 1 Collection)
-    """
-
-    # --- DOCTOR USE CASE (User A), examples ---
+    # DOCTOR USE CASE (Usecase 1)
     @abstractmethod
     def find_patients(self, query: str) -> list[dict]:
         pass
 
-    # get details plus all appointments
     @abstractmethod
     def get_patient_details(self, patient_id: int) -> dict:
         pass
@@ -31,28 +25,7 @@ class DatabaseStrategy(ABC):
     def get_doctor_report(self, start_year: int) -> list[dict]:
         pass
 
-    # --- CLERK USE CASE (User B), examples ---
-    # @abstractmethod
-    # def generate_revenue_report(self) -> dict: pass
-    #
-    # @abstractmethod
-    # def get_hospital_stats(self) -> dict: pass
-
-    # --- Shared functionality, implement in MongoBase/SQLBase
+    # Shared functionality, implemented in MongoBase/SQLBase
     @abstractmethod
     def _get_connection(self):
         pass
-
-    """
-    @abstractmethod
-    def get_by_id(self, table_name: str, record_id: int):
-        pass
-
-    @abstractmethod
-    def insert(self, table_name: str, data: dict):
-        pass
-
-    @abstractmethod
-    def update(self, table_name: str, record_id: int, data: dict):
-        pass
-    """
