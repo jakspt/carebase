ed017b38640174f67439549e8525cfb581b23331
Jakob Sapototzky <32401158+jakspt@users.noreply.github.com>
2026-01-11 00:14:47 +0100

    added current DB in navbar

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,30 @@
+import os
+
 from flask import Flask
+
+from app.db import MongoDBStrategy, get_db
 
 
 # Factory for creating new app
 def create_app():
     app = Flask(__name__)
 
-    # TODO: Make this an actual secret key.
-    app.secret_key = 'secret'
+    app.secret_key = os.urandom(24)
 
-    from app.core import core_bp
+    @app.context_processor
+    def inject_db_type():
+        # This runs before every template render and checks the DB currently in use
+        current_strategy = get_db()
+
+        if isinstance(current_strategy, MongoDBStrategy):
+            return dict(current_db="MongoDB")
+        else:
+            return dict(current_db="MariaDB")
+
     from app.admin import admin_bp
-    from app.doctor import doctor_bp
     from app.clerk import clerk_bp
+    from app.core import core_bp
+    from app.doctor import doctor_bp
 
     app.register_blueprint(core_bp)
     app.register_blueprint(admin_bp)
