474d16c5399862db8348b03d4223f8ac6740b80b
Jakob Sapototzky <32401158+jakspt@users.noreply.github.com>
2026-09-19 22:47:53 +0200

    add navigation

--- frontend/carebase-frontend/src/app/header-layout/header-layout.ts
+++ frontend/carebase-frontend/src/app/header-layout/header-layout.ts
@@ -2,11 +2,28 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatAnchor } from '@angular/material/button';
 
 @Component({
-  imports: [RouterOutlet, MatToolbarModule, MatIconModule],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatAnchor,
+  ],
   selector: 'app-header-layout',
   styleUrl: './header-layout.css',
   templateUrl: './header-layout.html',
 })
-export class HeaderLayout {}
+export class HeaderLayout {
+  switchRole(user: string) {
+    // switching happens here
+  }
+  logout() {
+    //logging out happens here
+  }
+}
